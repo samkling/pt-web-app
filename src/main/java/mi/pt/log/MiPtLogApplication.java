e77c37c071931cd1798b0682758e02e70a4e4400
Sam Kling <samtkling@gmail.com>
2018-09-23 22:16:24 -0400

    Initial Commit for spring backend -- may not build yet

---
+++ src/main/java/mi/pt/log/MiPtLogApplication.java
@@ -0,0 +1,12 @@
+package mi.pt.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPtLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPtLogApplication.class, args);
+    }
+}
